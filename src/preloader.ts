81b63412043a74c4e3cb2a194dbfab7b9a898791
Brandon DeRosier <x@bdero.me>
2018-12-01 16:11:24 -0800

    Progress information from preloader

--- src/preloader.ts
+++ src/preloader.ts
@@ -8,6 +8,17 @@ interface AssetLibrary {
     [assetName: string]: AudioBuffer
 }
 
+interface ProgressStats {
+    /** The total number of items enqueued for preloading. */
+    readonly totalItems: number
+    /** The total number of items. */
+    readonly loadedItems: number
+    /** Floating point range between 0 and 1. */
+    readonly percentage: number
+}
+
+declare type ProgressCallback = (progress: ProgressStats) => void
+
 function getFileType(url: string): string | null {
     const split = url.split('.')
     if (split.length < 2) {
@@ -22,53 +33,98 @@ function getFileType(url: string): string | null {
  */
 class Preloader {
     private preloadAssets: AssetURLs = {}
+    private loadedItems: AssetLibrary = {}
+    private preloadPromise: Promise<Readonly<AssetLibrary>> | null = null
+
+    private numTotalItems: number = 0
+    private numLoadedItems: number = 0
+
+    private progressCallback: ProgressCallback | null = null
+
+    constructor(progressCallback?: ProgressCallback) {
+        this.progressCallback = progressCallback || null
+    }
+
+    isPreloading() {
+        return this.preloadPromise !== null
+    }
 
     addPreloadItems(items: AssetURLs) {
-        this.preloadAssets = {...this.preloadAssets, ...items}
+        if (this.isPreloading()) {
+            throw new Error(
+                'Unable to enqueue items to a preloader that has already ' +
+                'started fetching.')
+        }
+        this.numTotalItems += Object.keys(items).length
+        this.preloadAssets = Object.assign(this.preloadAssets, items)
     }
-    fetch(): Promise<AssetLibrary> {
-        const promises: Array<Promise<AssetLibrary>> = []
-        Object.keys(this.preloadAssets).forEach(assetName => {
-            const url = this.preloadAssets[assetName]
-            promises.push(
-                fetch(url)
-                .then(result => {
-                    if (result.status !== 200) {
-                        return Promise.reject(
-                            `Fetch for URL "${result.url}" returned ` +
-                            `unexpected status code "${result.status}".`)
-                    }
-                    const fileType = getFileType(result.url)
-                    if (fileType === null) {
+
+    getProgress(): ProgressStats {
+        return {
+            totalItems: this.numTotalItems,
+            loadedItems: this.numLoadedItems,
+            percentage: this.numLoadedItems/this.numTotalItems
+        }
+    }
+
+    fetch(): Promise<Readonly<AssetLibrary>> {
+        if (this.preloadPromise !== null) return this.preloadPromise
+
+        this.preloadPromise = new Promise((resolve, reject) => {
+            const promises: Array<Promise<void>> = []
+            Object.keys(this.preloadAssets).forEach(assetName => {
+                const url = this.preloadAssets[assetName]
+                promises.push(
+                    fetch(url)
+                    .then(result => {
+                        if (result.status !== 200) {
+                            return Promise.reject(
+                                `Fetch for URL "${result.url}" returned ` +
+                                `unexpected status code "${result.status}".`)
+                        }
+                        const fileType = getFileType(result.url)
+                        if (fileType === null) {
+                            return Promise.reject(
+                                `Unable to preload asset "${result.url}" ` +
+                                `with no file type.`)
+                        }
+
+                        if (fileType === 'ogg') {
+                            return (
+                                result.arrayBuffer()
+                                .then(value => audioContext.decodeAudioData(
+                                    value))
+                                .then(buffer => {
+                                    return {[assetName]: buffer}
+                                })
+                            )
+                        }
+
                         return Promise.reject(
                             `Unable to preload asset "${result.url}" with ` +
-                            `no file type.`)
-                    }
+                            `unknown file type "${fileType}".`)
+                    })
+                    .then((items: AssetLibrary) => {
+                        Object.keys(items).forEach((name) => {
+                            this.numLoadedItems += 1
+                            this.loadedItems[name] = items[name]
+                        })
+                        if (this.progressCallback !== null) {
+                            this.progressCallback(this.getProgress())
+                        }
+                    })
+                )
+            });
 
-                    if (fileType === 'ogg') {
-                        return (
-                            result.arrayBuffer()
-                            .then(value => audioContext.decodeAudioData(value))
-                            .then(buffer => {
-                                return {[assetName]: buffer} as AssetLibrary
-                            })
-                        )
-                    }
-
-                    return Promise.reject(
-                        `Unable to preload asset "${result.url}" with ` +
-                        `unknown file type "${fileType}".`)
-                })
-            )
-        });
-        return (
+            // Resolve or reject the promise once all of the items are loaded,
+            // or at least one of them fails.
             Promise.all(promises)
-            .then(values => {
-                if (values.length === 0) return {}
-                if (values.length < 2) return values[0]
-                return Object.assign(values[0], ...values.slice(1))
-            })
-        )
+            .then(_ => this.loadedItems as Readonly<AssetLibrary>)
+            .then(value => resolve(value))
+            .catch(value => reject(value))
+        })
+
+        return this.preloadPromise
     }
 }
 
